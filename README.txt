c3df584934f768ae3d5999bf7e45e2cf80d43a20
S-s-Spudd <77349475+S-s-Spudd@users.noreply.github.com>
2022-03-29 16:15:07 +0100

    Initial commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+Hosting Domain on "netlify.com"
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/7966c46f-0408-4645-b7e2-3a740932be54/deploy-status)](https://app.netlify.com/sites/edscollegeassignment/deploys)
---
+++ assets/js/EmailValidationCheck.js
@@ -0,0 +1,24 @@
+function validation()
+    {
+        var form = document.getElementById("form");
+        var email = document.getElementById("email").value;
+        var text = document.getElementById("text");
+        var pattern = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+        if (email.match(pattern))
+        {
+            form.classList.add("valid");
+            form.classList.remove("invalid");
+            text.innerHTML = "Your Email Address is valid."
+            text.style = "'00ff00"
+        }
+        else
+        {
+            form.classList.add("valid");
+            form.classList.remove("invalid");
+            text.innerHTML = "Please enter a valid Email Address."
+            text.style = "'ff0000"
+            alert("Not a Valid Email Address");
+        }
+        
+    }
